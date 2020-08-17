bc97cec4dcbdd5b3c6b7c7586ff3cdb52ffd3f4a
Ladislav Dobrovsky <ladislav.dobrovsky@gmail.com>
2020-08-17 22:12:29 +0200

    WIP: ItemProperties moved to item_properties.ui, edit properties

--- editor/widgets.py
+++ editor/widgets.py
@@ -1,3 +1,4 @@
+from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from typing import Union
@@ -14,13 +15,16 @@ class ItemProperties(QStackedWidget):
 
     def __init__(self, parent):
         super().__init__(parent)
+
+        uic.loadUi('editor/item_properties.ui', self)
+
         self.main_window = self.window()
         self.is_filling_forms = False
 
     def after_init(self):
         self.is_filling_forms = True
         for name, _ in ItemProperties.TimedPDists:
-            self.main_window.transition_timed_pdist_comboBox.addItem(name)
+            self.transition_timed_pdist_comboBox.addItem(name)
         self.is_filling_forms = False
 
     def edits_enabled(self, enabled):
@@ -32,33 +36,44 @@ class ItemProperties(QStackedWidget):
     def item_selected(self, item):
         editor = self.main_window.editor
         if item is None:
-            self.setCurrentWidget(self.main_window.empty_props_page)
+            self.setCurrentWidget(self.empty_props_page)
         elif isinstance(item, PlaceItem):
             self.is_filling_forms = True
-            self.setCurrentWidget(self.main_window.place_props_page)
-            self.main_window.place_name_lineEdit.setText(item.place.name)
-            self.main_window.place_init_tokens_spinBox.setValue(item.place.capacity)
-            self.main_window.place_capacity_spinBox.setValue(item.place.capacity)
-            self.main_window.place_infinite_capacity_label.setText(f'(infinite capacity = {Place.INF_CAPACITY})')
+            self.setCurrentWidget(self.place_props_page)
+            self.place_name_lineEdit.setText(item.place.name)
+            self.place_init_tokens_spinBox.setValue(item.place.capacity)
+            self.place_capacity_spinBox.setValue(item.place.capacity)
+            self.place_infinite_capacity_label.setText(f'(infinite capacity = {Place.INF_CAPACITY})')
             self.is_filling_forms = False
 
         elif isinstance(item, TransitionItem):
-            self.setCurrentWidget(self.main_window.transition_props_page)
-            transition_types_stackedWidget = self.main_window.transition_types_stackedWidget
-            self.main_window.transition_name_lineEdit.setText(item.transition.name)
-            if type(item.transition) == Transition:
-                transition_types_stackedWidget.setCurrentWidget(self.main_window.transition_normal_page)
-            elif type(item.transition) == TransitionPriority:
-                transition_types_stackedWidget.setCurrentWidget(self.main_window.transition_priority_page)
+            self.setCurrentWidget(self.transition_props_page)
+            transition_types_stackedWidget = self.transition_types_stackedWidget
+            self.is_filling_forms = True
+            self.transition_name_lineEdit.setText(item.transition.name)
 
+            # TODO: create lookup for classes and displayed names (decouple from gui inside .ui file)
+            self.transition_type_comboBox.setCurrentText(item.transition.__class__.__name__)
+            if type(item.transition) == Transition:
+                transition_types_stackedWidget.setCurrentWidget(self.transition_normal_page)
+            elif type(item.transition) == TransitionPriority:
+                transition_types_stackedWidget.setCurrentWidget(self.transition_priority_page)
+                self.transition_priority_spinBox.setValue(item.transition.priority)
             elif type(item.transition) == TransitionTimed:
-                transition_types_stackedWidget.setCurrentWidget(self.main_window.transition_timed_page)
+                transition_types_stackedWidget.setCurrentWidget(self.transition_timed_page)
+                self.transition_t_min_doubleSpinBox.setValue(item.transition.t_min)
+                self.transition_t_max_doubleSpinBox.setValue(item.transition.t_max)
             elif type(item.transition) == TransitionStochastic:
-                transition_types_stackedWidget.setCurrentWidget(self.main_window.transition_stochastic_page)
+                transition_types_stackedWidget.setCurrentWidget(self.transition_stochastic_page)
+                self.transition_probability_percent_spinBox.setValue(item.transition.probability*100)
+            self.is_filling_forms = False
         elif isinstance(item, ArcItem):
-            self.setCurrentWidget(self.main_window.arc_props_page)
-            self.main_window.arc_name_lineEdit.setText(item.arc.name)
-            self.main_window.arc_n_tokens_spinBox.setValue(item.arc.n_tokens)
+            self.setCurrentWidget(self.arc_props_page)
+            self.is_filling_forms = True
+            self.arc_name_lineEdit.setText(item.arc.name)
+            self.arc_n_tokens_spinBox.setValue(item.arc.n_tokens)
+            self.arc_inhibitor_checkBox.setChecked(type(item.arc) != Arc)
+            self.is_filling_forms = False
 
     def place_name_changed(self, name):
         if not self.is_filling_forms:
@@ -98,11 +113,35 @@ class ItemProperties(QStackedWidget):
             editor.selected.change_transition_type(cls)
             self.item_selected(editor.selected)
 
-    def transition_timed_pdist_changed(self, idx):
+    def transition_priority_changed(self, value: int):
+        if not self.is_filling_forms:
+            editor = self.main_window.editor
+            editor.selected.transition.priority = value
+            editor.selected.update_texts()
+
+    def transition_pdist_changed_idx(self, idx: int):
         if not self.is_filling_forms:
             editor = self.main_window.editor
             name, func = ItemProperties.TimedPDists[idx]
             editor.selected.set_timed_pdist(func)
+            editor.selected.update_texts()
+
+    def transition_t_min_changed(self, value: float):
+        if not self.is_filling_forms:
+            editor = self.main_window.editor
+            editor.selected.transition.t_min = value
+            editor.selected.update_texts()
+
+    def transition_t_max_changed(self, value: float):
+        if not self.is_filling_forms:
+            editor = self.main_window.editor
+            editor.selected.transition.t_max = value
+            editor.selected.update_texts()
+
+    def transition_probability_percent_changed(self, value: int):
+        if not self.is_filling_forms:
+            editor = self.main_window.editor
+            editor.selected.transition.probability = value / 100.0
             editor.selected.update_texts()
 
     def arc_name_changed(self, name: str):
@@ -116,6 +155,6 @@ class ItemProperties(QStackedWidget):
             editor.selected.arc.n_tokens = value
             editor.selected.update_texts()
 
-    def arc_inhibitor_checkbox_toggled(self, toggled: bool):
+    def arc_inhibitor_toggled(self, toggled: bool):
         if not self.is_filling_forms:
             print('TODO: arc/inhibitor switch')
